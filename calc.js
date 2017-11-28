478b2c33ec53dfd324b0d84826331b2bae24a897
Revanee <hkamenov123@gmail.com>
2017-11-28 11:11:44 +0100

    Core functionality

--- calc.js
+++ calc.js
@@ -1,9 +1,15 @@
 function calc (data) {
-    data = data.split('+')
+    data = data.split('')
 
-    data = data.reduce((num, acc) => {
-        acc += Number(num)
-    }, 0)
+    data = data.reduce((arr, sym) => {
+        if (Number(sym)) arr[arr.length - 1].push(sym)
+        else (arr.push([sym]))
+        return arr
+    }, [[]])
+
+    data = data.map(x => x.join(''))
+
+    data = data.reduce((acc, num) => acc + Number(num), 0)
 
     return data
 }
--- calc.test.js
+++ calc.test.js
@@ -1,10 +1,25 @@
 const calc = require('./calc')
 
-if(calc('0') !== 0) {
-    console.log(calc('0'))
-    throw Error("Literal parse")
-}
-if(calc('5') !== 5) throw Error("Literal parse")
-if(calc('-5') !== -5) throw Error("Literal parse")
+test('Literal 0', () => {
+    expect(calc('0')).toBe(0)
+})
 
-if(calc("1+1") !== 2) throw Error("Addition")
+test('Literal 5', () => {
+    expect(calc('5')).toBe(5)
+})
+
+test('Literal -5', () => {
+    expect(calc('-5')).toBe(-5)
+})
+
+test('Addition 1+1', () => {
+    expect(calc('1+1')).toBe(2)
+})
+
+test('Subtraction 2-1', () => {
+    expect(calc('2-1')).toBe(1)
+})
+
+test('Addition and subtraction 2+5-1-2+4', () => {
+    expect(calc('2+5-1-2+4')).toBe(8)
+})
